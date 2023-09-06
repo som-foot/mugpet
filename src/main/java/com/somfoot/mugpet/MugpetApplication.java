b8250de76e80b1a9361053507909c3933594a245
Chony <chdms7471@naver.com>
2023-09-06 23:27:34 +0900

    init

---
+++ src/main/java/com/somfoot/mugpet/MugpetApplication.java
@@ -0,0 +1,13 @@
+package com.somfoot.mugpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugpetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MugpetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/somfoot/mugpet/MugpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somfoot.mugpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugpetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
