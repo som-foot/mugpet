e4bcf0515c1c4ddb78d5f5d63eca310daf85732b
Chony <chdms7471@naver.com>
2023-09-18 20:08:47 +0900

    itemEntity&Repository

--- src/main/java/com/somfoot/mugpet/entity/Item.java
+++ src/main/java/com/somfoot/mugpet/entity/Item.java
@@ -1,8 +1,18 @@
 package com.somfoot.mugpet.entity;
 
-public class Item {
+import jakarta.persistence.*;
+import lombok.Getter;
 
-	private int item_id;				//primary key
+import java.io.Serializable;
+
+@Entity
+@Table(name = "ITEM")
+@Getter
+public class Item implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long item_id;				//primary key
 	private int category_id;			//item이 속한 category id
 	private int spe_id;					//반려동물 종 id
 	private String itemName;			//상품명
@@ -22,70 +32,6 @@ public class Item {
 		this.price = price;
 		this.brand = brand;
 		this.imageUrl = imageUrl;
-		this.comments = comments;
-	}
-
-	public int getItem_id() {
-		return item_id;
-	}
-
-	public void setItem_id(int item_id) {
-		this.item_id = item_id;
-	}
-
-	public int getCategory_id() {
-		return category_id;
-	}
-
-	public void setCategory_id(int category_id) {
-		this.category_id = category_id;
-	}
-
-	public int getSpe_id() {
-		return spe_id;
-	}
-
-	public void setSpe_id(int spe_id) {
-		this.spe_id = spe_id;
-	}
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
 		this.comments = comments;
 	}
 
