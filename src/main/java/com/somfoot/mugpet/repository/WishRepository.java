214d4c2cbc18bec4e1902b268e1b0a5848e88a86
smjun0526 <smjun0526@naver.com>
2023-09-25 22:21:45 +0900

    create cartItem, wish queries

---
+++ src/main/java/com/somfoot/mugpet/repository/WishRepository.java
@@ -0,0 +1,20 @@
+package com.somfoot.mugpet.repository;
+
+import com.somfoot.mugpet.entity.Cart;
+import com.somfoot.mugpet.entity.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface WishRepository extends JpaRepository<Wish, Long> {
+
+    //insert?
+
+    //delete item depends on user
+
+    public void deleteWishByIdAndUser(@Param("itemId")long item_id, @Param("uId")long u_id);
+
+    //select
+    public Optional<Wish> findById(@Param("memberId")long member_id);
+}
