d9fdc4b65981b1ade9be1bc28155c45c5813ccb3
Chony <chdms7471@naver.com>
2023-10-09 13:36:46 +0900

    1009

--- src/main/java/com/somfoot/mugpet/entity/MemberInfo.java
+++ src/main/java/com/somfoot/mugpet/entity/MemberInfo.java
@@ -4,6 +4,7 @@ import com.somfoot.mugpet.dto.MemberFormDto;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,8 +13,8 @@ import java.util.List;
 
 @Entity
 @Table(name = "member")
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor
 public class MemberInfo {
 
 	@Id
@@ -35,10 +36,16 @@ public class MemberInfo {
 	private List<Pet> pets = new ArrayList<>();
 	private String role;
 
+	//N:1 양방향 연관관계
+	@OneToMany(mappedBy = "LineItem")
+	private List<LineItem> lineItemList;
+
+	//N:1 양방향 연관관계
+	@OneToMany(mappedBy = "Orders")
+	private List<LineItem> orderList;
 
 	@Builder
 	public MemberInfo (long id,String nickname,String name,String email, String password, String phoneNum,String address,int point,String role){
-
 		this.id = id;
 		this.nickname = nickname;
 		this.name = name;
